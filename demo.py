96a395891747b4aa0d062677a1b0595568792bb3
Yuxiang Yang <yxyang@berkeley.edu>
2017-10-02 21:47:32 -0700

    updated demo

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@ from finitefield.finitefield import FiniteField
 
 ## Setup ##
 n = 3   # Number of Packets we want to send
-k = 1   # Maximum number of errors we can tolerate
+k = 2   # Maximum number of errors we can tolerate
 p = 11   # We're working on finite field modulo p
 Fp = FiniteField(p)
 enc, dec, solveSystem = makeEncoderDecoder(n + 2 * k, n, p)
@@ -17,8 +17,8 @@ print(encoded)
 
 ## Decode Message like this ##
 received = deepcopy(encoded)
-received[0]=Fp(3) #Changing a message in received.
-received[1]=Fp(7) #Changing a message in received.
+received[0]=Fp(2) #Changing a message in received.
+received[1]=Fp(1) #Changing a message in received.
 received[2]=Fp(0) #Changing a message in received.
 received[3]=Fp(2) #Changing a message in received.
 received[4]=Fp(10) #Changing a message in received.
